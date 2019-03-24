a4bddd3e278f161e1442b335bb8874c25453e2c8
Adriana <adrianaorellana34@gmail.com>
2019-03-24 15:41:02 -0400

    Cambios en el mensaje

--- src/application/ClienteController.java
+++ src/application/ClienteController.java
@@ -50,11 +50,18 @@ public class ClienteController implements Runnable{
 	
 	public void enviarSolicitud() {
 		try {
-			Socket cliente = new Socket("192.168.0.23", 52000);
+			Socket cliente = new Socket("192.168.1.118", 52000);
 			
 			DataOutputStream flujo_salida = new DataOutputStream(cliente.getOutputStream());
 			
-			String mensaje = "MSH|^~\\&|HIS|MedCenter|LIS|MedCenter|20060307110114||ORM^O01|MSGID20060307110114|P|2.3\rPID|||12001||Jones^John^^^Mr.||19670824|M|||123 West St.^^Denver^CO^80020^USA|||||||\rPV1||O|OP^PAREG^||||2342^Jones^Bob|||OP|||||||||2|||||||||||||||||||||||||20060307110111|\rORC|NW|20060307110114\rOBR|1|20060307110114||003038^Urinalysis^L|||20060307110114";
+			//String mensaje = "MSH|^~\\&|HIS|MedCenter|LIS|MedCenter|20060307110114||ORM^O01|MSGID20060307110114|P|2.3\rPID|||12001||Jones^John^^^Mr.||19670824|M|||123 West St.^^Denver^CO^80020^USA|||||||\rPV1||O|OP^PAREG^||||2342^Jones^Bob|||OP|||||||||2|||||||||||||||||||||||||20060307110111|\rORC|NW|20060307110114\rOBR|1|20060307110114||003038^Urinalysis^L|||20060307110114";
+			String mensaje = "MSH|^~\\&|EPIC|EPIC|||20140418173314|1148|ORM^O01|497|D|2.3||\r" + 
+					"PID|1||20891312^^^^EPI||APPLESEED^JOHN^A^^MR.^||19661201|M||AfrAm|505 S. HAMILTON AVE^^MADISON^WI^53505^US^^^DN |DN|(608)123-4567|(608)123-5678||S|| 11480003|123-45-7890||||^^^WI^^\r" + 
+					"PD1|||FACILITY(EAST)^^12345|1173^MATTHEWS^JAMES^A^^^\r" + 
+					"PV1|||^^^CARE HEALTH SYSTEMS^^^^^||| |1173^MATTHEWS^JAMES^A^^^||||||||||||610613||||||||||||||||||||||||||||||||V\r" + 
+					"ORC|NW|987654^EPIC|76543^EPC||Final||^^^20140418170014^^^^||20140418173314|1148^PATTERSON^JAMES^^^^||1173^MATTHEWS^JAMES^A^^^|1133^^^222^^^^^|(618)222-1122||\r" + 
+					"OBR|1|363463^EPC|1858^EPC|73610^X-RAY ANKLE 3+ VW^^^X-RAY ANKLE ||||||||||||1173^MATTHEWS^JAMES^A^^^|(608)258-8866||||||||Final||^^^20140418170014^^^^|||||6064^MANSFIELD^JEREMY^^^^||1148010^1A^EAST^X-RAY^^^|^|\r" + 
+					"DG1||I10|S82^ANKLE FRACTURE^I10|ANKLE FRACTURE||";
 			//String mensaje = txtMensaje.getText();
 						
 			flujo_salida.writeUTF(mensaje);
